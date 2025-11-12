df5ba08f5cf6b09806bee36fad40834eb3af47c6
Renukumar <renukumark3r@gmail.com>
2025-11-12 16:53:05 +0530

    Initial push

---
+++ src/test/java/Modules/BranchCreation.java
@@ -0,0 +1,134 @@
+package Modules;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BranchCreation {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ChromeOptions op=new ChromeOptions();
+		op.setAcceptInsecureCerts(true);
+		ChromeDriver d=new ChromeDriver(op);
+		d.get("https://192.9.200.27:2322/IB_12_4/login.htm");
+		d.findElement(By.xpath("//input[@id='userid']")).sendKeys("NCS10");
+		d.findElement(By.xpath("//input[@type='password']")).sendKeys("1234");
+		d.findElement(By.xpath("//button[@type='button']")).click();
+		//Alert alert = d.switchTo().alert();
+		//alert.accept();
+
+		Thread.sleep(3000);
+		d.findElement(By.xpath("//img[@title='Master']")).click();
+		Thread.sleep(3000);
+		d.findElement(By.xpath("//h4[@class='cls_sidebar_menu_text' and contains(., 'Auditee')]")).click();
+		//d.findElements(By.partialLinkText("listbranch.htm?menuOptId=501"))
+		WebElement branchMaster = d.findElement(By.linkText("Branch Master"));
+		branchMaster.click();
+		d.findElement(By.xpath("//button[@onclick='addbranch();']")).click();
+		d.findElement(By.xpath("//input[@id='brancd']")).sendKeys("101123");
+		d.findElement(By.xpath("//input[@id='brannm']")).sendKeys("Palur");
+		Select branch = new Select(d.findElement(By.xpath("//select[@id='mainCd']")));
+		branch.selectByValue("MAIN");
+		d.findElement(By.xpath("//input[@id='address1']")).sendKeys("Chennai");
+		JavascriptExecutor js = (JavascriptExecutor) d;
+		js.executeScript("window.scrollBy(0,500)");
+		d.findElement(By.xpath("//input[@id='bsrcode']")).sendKeys("1234554");
+		d.findElement(By.xpath("//input[@id='opendate']")).click();
+		/*
+		 * WebDriverWait wait = new WebDriverWait(d, Duration.ofSeconds(10));
+		 * wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(
+		 * "ui-datepicker-calendar")));
+		 */
+
+		// Select Year
+		Thread.sleep(3000);
+		Select year = new Select(d.findElement(By.xpath("//select[@title='Change the year']")));
+		year.selectByVisibleText("2025");
+
+		// Select Month
+		Select month = new Select(d.findElement(By.xpath("//select[@title='Change the month']")));
+		month.selectByVisibleText("November");
+
+		// Select Day
+	List<WebElement> day = d.findElements(By.xpath("//div[@class='datepick-month']//tbody//td"));
+	
+	for(WebElement dd: day) {
+		if(dd.getText().equalsIgnoreCase("5")) {
+			dd.click();
+			break;
+		}
+	}
+		Select BRType =new Select(d.findElement(By.xpath("//select[@id='catogry']")));
+		BRType.selectByVisibleText("Rural");
+		Select RBIAtype=new Select(d.findElement(By.xpath("//select[@id='rbiaType']")));
+		RBIAtype.selectByVisibleText("REGULAR");
+		d.findElement(By.xpath("//input[@id='city']")).sendKeys("Chennai");
+		d.findElement(By.xpath("//input[@id='pinNum']")).sendKeys("606908");
+		d.findElement(By.xpath("//tr[@id='st']//i")).click();
+		Thread.sleep(3000);
+		
+		 List<WebElement> State = d.findElements(By.xpath("//div[@class='popupcontent p-b-b']//ul[@class='listitem']/li"));		Thread.sleep(3000);
+
+		 for(WebElement st:State)
+		 {
+			 if(st.getText().equalsIgnoreCase("01-ASSAM")) {
+				 st.click();
+					 break;
+			 }
+		 }
+		 Thread.sleep(3000);
+		
+			
+			  d.findElement(By.xpath("//tr[@id='dist']//i")).click();
+			  List<WebElement>District = d.findElements(By.xpath("//div[@class='popupcontent p-b-b']//ul[@class='listitem']/li"));
+			  
+			  for(WebElement DI:District) { 
+				  if(DI.getText().contains("43-G"))
+				  { 
+					  DI.click();
+			  break;
+			  } } 
+			  Thread.sleep(3000);
+			  d.findElement(By.xpath("//input[@id='email']")).sendKeys("renukumar@gmail.com");
+			  d.findElement(By.xpath("//select[@id='forEx']")).click();
+			  Thread.sleep(3000);
+			  Select Foreign = new Select(d.findElement(By.xpath("//select[@id='forEx']")));
+			  Foreign.selectByVisibleText("A");
+			 d.findElement(By.xpath("//input[@id='fromval']")).sendKeys("12");
+			  d.findElement(By.xpath("//input[@id='toval']")).sendKeys("21");
+			  Select Weekly=new Select(d.findElement(By.xpath("//select[@id='wklyFull']")));
+			  Weekly.selectByVisibleText("Sunday");
+			  Thread.sleep(3000); 
+			
+			  d.findElement(By.xpath("//tr[@id='zo']//i")).click();
+			  Thread.sleep(3000);
+			  List<WebElement> Attached = d.findElements(By.xpath("//div[@class='popupcontent p-b-b']//ul[@class='listitem']/li"));
+			  for(WebElement AT:Attached)
+			  {
+				  if(AT.getText().contains("0001"))
+			  { AT.click();
+			  break; } }
+			  
+			  Thread.sleep(3000);
+		     d.findElement(By.xpath("//tr[@id='bc']//i")).click();
+		     List<WebElement>Category = d.findElements(By.xpath("//div[@class='popupcontent p-b-b']//ul[@class='listitem']/li"));
+		     for(WebElement CA:Category )
+		     {
+		    	 if(CA.getText().contains("PER-PERSONAL BANKING"))
+		    	 {CA.click();
+	}
+		    	 d.findElement(By.xpath("//button[@type='button' and contains(@onclick,'saveBranch') ]")).click();
+		 	//d.close();
+
+}}}
